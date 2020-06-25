ebe6d27d0e53570c04ca33628d22dd681cddc138
eugene231 <50164355+eugene231@users.noreply.github.com>
2020-06-25 16:39:38 +0900

    Project Create
    .

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    
+    public double possesionLight;
+    bool isMoving;
+    private int currentOwner;
+    PlayerStat player1;
+    PlayerStat player2;
+
+    void Awake() {
+        player1 = GameObject.Find("Player1").GetComponent<PlayerStat>();
+        player2 = GameObject.Find("Player2").GetComponent<PlayerStat>();
+        currentOwner = 0;
+        isMoving = false;
+    }
+
+    void Start()
+    {
+     
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Production() {
+        if(currentOwner==1) {
+            possesionLight += player1.perLightProduction;
+        }
+        if(currentOwner==2) {
+
+            possesionLight += player2.perLightProduction;
+        }
+    }
+
+    void Coming(int playerN, double lightSpeed) {
+        if(playerN==currentOwner) {
+            possesionLight += lightSpeed;
+        }
+        else {
+            possesionLight -= lightSpeed;
+        }
+    }
+
+    void Moving() {
+       if(isMoving==false) {
+           
+            //연결될 행성의 Coming(currnetOwner,lightSpeed);
+       }
+    }
+   
+}
---
+++ Assets/Scripts/PlayerStat.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : MonoBehaviour
+{
+
+    public double currentGold;
+    public double currentLight;
+    public double perGoldProduction;
+    public double perLightProduction;
+    public double lightSpeed;
+    
+    void Awake() 
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    
+}
