e963780ae632dbed88b36ac364cdc33c0388cbb3
eugene231 <50164355+eugene231@users.noreply.github.com>
2020-06-28 18:29:50 +0900

    Revert "Revert "Merge branch 'master' into Effects""
    This reverts commit 5f992c2ee572f690233dbcee8896cb6700ef7f05.

---
+++ Assets/Scripts/UI/RoomChoiceUI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomChoiceUI : MonoBehaviour
+{
+
+    Server server;
+    RoomCodeUI roomCodeUi;
+
+
+    void Awake()
+    {
+        server = GameObject.Find("MakingServer").GetComponent<Server>();
+        roomCodeUi = GameObject.Find("RoomCodeUI").GetComponent<RoomCodeUI>();
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void MatchingOnClick()
+    {
+        server.ToServer("TO_SERVER JOIN AUTO");
+    }
+
+    public void MakingRoomOnClick()
+    {
+        //방 만드는 UI 띄우기
+    }
+
+    public void RoomSearchOnClick()
+    {
+        //방 코드 입력하는 UI 띄우기 
+        roomCodeUi.OpenRoomCodeUI();
+        Debug.Log("open roomcode UI");
+
+    }
+
+    public void closeButtonOnClick()
+    {
+        roomCodeUi.CloseRoomCodeUI();
+    }
+
+    void JoinRoom()
+    {
+
+    }
+
+
+}
